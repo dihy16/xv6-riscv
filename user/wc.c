285dbc016900f584862b45dfeb181640640c5abe
dihy16 <dinhhogiahuy290605@gmail.com>
2026-01-30 09:19:38 +0000

    feat: add options arguments to wc

--- user/wc.c
+++ user/wc.c
@@ -4,6 +4,9 @@
 #include "user/user.h"
 
 char buf[512];
+int flag_l = 0;
+int flag_c = 0;
+int flag_w = 0;
 
 void
 wc(int fd, char *name)
@@ -30,20 +33,40 @@ wc(int fd, char *name)
     printf("wc: read error\n");
     exit(1);
   }
-  printf("%d %d %d %s\n", l, w, c, name);
+  if (flag_l) printf("%d ", l);
+  if (flag_w) printf("%d ", w);
+  if (flag_c) printf("%d ", c);
+  printf("%s\n", name);
 }
 
 int
 main(int argc, char *argv[])
 {
   int fd, i;
+  int files_start = argc;
+  for(i = 1; i < argc; i++){
+    if (argv[i][0] == '-') {
+      if (strchr(argv[i], 'l')) flag_l = 1;
+      if (strchr(argv[i], 'c')) flag_c = 1;
+      if (strchr(argv[i], 'w')) flag_w = 1;
+    }
+    else {
+      files_start = i;
+      break;
+    }
+  }
 
-  if(argc <= 1){
+  // if no flag argument, print all lines/words/characters 
+  if (!flag_l && !flag_c && !flag_w) {
+    flag_l = flag_c = flag_w = 1;
+  }
+  
+  if(files_start >= argc){
     wc(0, "");
     exit(0);
   }
 
-  for(i = 1; i < argc; i++){
+  for(i = files_start; i < argc; i++){
     if((fd = open(argv[i], O_RDONLY)) < 0){
       printf("wc: cannot open %s\n", argv[i]);
       exit(1);
