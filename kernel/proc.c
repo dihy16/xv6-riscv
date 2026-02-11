00f95cc465e3e2e7473f0c6b04da4a927e5a4011
dihy16 <dinhhogiahuy290605@gmail.com>
2026-02-11 04:20:35 +0000

    convert kernel procstate to user procstate

--- kernel/proc.c
+++ kernel/proc.c
@@ -692,6 +692,21 @@ procdump(void)
   }
 }
 
+static enum procstate_u
+map_state(enum procstate s)
+{
+  switch (s) {
+  case UNUSED:   return U_UNUSED;
+  case USED:     return U_USED;
+  case SLEEPING: return U_SLEEPING;
+  case RUNNABLE: return U_RUNNABLE;
+  case RUNNING:  return U_RUNNING;
+  case ZOMBIE:   return U_ZOMBIE;
+  }
+  return U_UNUSED;
+}
+
+
 int
 getprocs(uint64 uaddr, int max)
 {
@@ -707,7 +722,7 @@ getprocs(uint64 uaddr, int max)
 
     if(p->state != UNUSED){
       pi.pid = p->pid;
-      pi.state = (enum procstate_u)p->state;
+      pi.state = map_state(p->state);
       pi.sz = p->sz;
       safestrcpy(pi.name, p->name, sizeof(pi.name));
 
